607f1ad326cc6bab18e464515c5faa12661b1511
Guilherme Perinotti <guilherme9115@hotmail.com>
2022-12-08 08:07:05 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.urlencoded({extended: true}));
+
+app.use((req: Request, res: Response) => {
+    res.status(404);
+    res.json({error: 'Endpoint não encontrado.'});
+});
+
+app.listen(process.env.PORT);
