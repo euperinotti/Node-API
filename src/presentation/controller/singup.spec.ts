42d87737855ea10056638c1ff93a7021704bb7d0
Guilherme Perinotti <guilherme9115@hotmail.com>
2023-02-02 21:48:33 -0300

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controller/singup.spec.ts
+++ src/presentation/controller/singup.spec.ts
@@ -5,7 +5,6 @@ describe('SingUp Controller', () => {
     const sut = new SingUpController()
     const httpRequest = {
       body: {
-        name: 'any_name',
         email: 'any_email@email.com',
         password: 'any_password',
         passwordConfirm: 'any_password'
@@ -15,5 +14,20 @@ describe('SingUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing params: name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SingUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirm: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing params: email'))
   })
 })
